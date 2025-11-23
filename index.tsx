9537e43fcabbdca419ca51960c6d199ae7bb29ce
colchoesnippon <glproducoes2012@gmail.com>
2025-11-23 14:07:14 -0300

    feat: Add Google Ads tracking
    Integrates Google Ads conversion tracking by adding the necessary gtag.js script to the `index.html`. This enables measurement of ad campaign performance.

--- index.tsx
+++ index.tsx
@@ -2,21 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Google Tag
-const script1 = document.createElement("script");
-script1.async = true;
-script1.src = "https://www.googletagmanager.com/gtag/js?id=AW-955599478";
-document.head.appendChild(script1);
-
-const script2 = document.createElement("script");
-script2.innerHTML = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'AW-955599478');
-`;
-document.head.appendChild(script2);
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
